c58964589f0c7a34e95748fcbe599bd127140162
Huan Xiao <389031426@qq.com>
2023-11-22 17:11:10 +0100

    add arguments

--- parser.py
+++ parser.py
@@ -3,8 +3,10 @@ import argparse
 def parse_arguments():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     # datasets parameters
-    parser.add_argument("--dataset", type=str, default=moscoco,
-                        help="choose which dataset?")
-
+    parser.add_argument("--dataset", default="flickr", choices=["flickr", "coco"], help="Choose the dataset to process (flickr/coco)")
+    # parser.add_argument("--root", default='/kaggle/input/flickr30k/Images', help="Root directory of the dataset")
+    # parser.add_argument("--ann_file", default="/kaggle/input/flickr30k/captions.txt", help="Annotation file of the dataset")
+    parser.add_argument('--evaluate', action='store_true')
+    
     args = parser.parse_args()
     return args

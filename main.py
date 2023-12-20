97c13afefa86fcf742a5be4bfb8f2d78b35f6fcf
Huan Xiao <389031426@qq.com>
2023-12-20 15:50:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -131,10 +131,10 @@ def main(args):
             targets_images = torch.arange(args.batch_size)
             targets_texts = targets_images.repeat_interleave(5)
 
-            print(logits_per_image.shape)
-            print(logits_per_text.shape)
-            print(targets_images.shape)
-            print(targets_texts.shape)
+            # print(logits_per_image.shape)
+            # print(logits_per_text.shape)
+            # print(targets_images.shape)
+            # print(targets_texts.shape)
 
             image_loss = contrastive_loss(logits_per_image , targets_images)
             text_loss = contrastive_loss(logits_per_text , targets_texts)

d0425193bb53cd1e242ffd18500e4cab8b9da679
Huan Xiao <389031426@qq.com>
2023-12-21 18:33:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -63,15 +63,11 @@ def contrastive_loss(logits_per_image, logits_per_text, margin=1.0):
 
     # #print(negative_loss_image)
 
-    text_i_matrix = torch.eye(len(logits_per_image)).repeat_interleave(5)
-    print(text_i_matrix.shape)
+    text_i_matrix = torch.eye(len(logits_per_image)).repeat_interleave(5,dim=0)
     image_i_matrix = torch.transpose(text_i_matrix, 0, 1)
 
     distance_per_image = margin - logits_per_image
     distance_per_text = margin - logits_per_text
-
-    print(distance_per_image.shape)
-    print(image_i_matrix.shape)
 
     positive_loss_image = (distance_per_image * image_i_matrix).mean()
     positive_loss_text = (distance_per_text * text_i_matrix).mean()

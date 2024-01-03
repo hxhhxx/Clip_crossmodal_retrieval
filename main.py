6ccb30fa9951176b0a5a5227dafc5264f0f35618
Huan Xiao <389031426@qq.com>
2024-01-03 12:43:40 +0100

    version 12/22

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from tqdm import tqdm
-import torch
 import torch.nn as nn
 from torch.nn import functional as F
 import torch.optim as optim
+from pytorch_metric_learning import losses
 import clip
 from torch.utils.data import random_split
 from Datasets.Flickr30k import Flickr30k
@@ -10,7 +9,6 @@ from Datasets.Flickr30k import Flickr30k
 from torch.utils.data import DataLoader
 import Evaluation
 import parser
-import itertools
 
 def split_dataset(args, preprocess, target_transform):
     if args.dataset == "flickr":
@@ -30,44 +28,58 @@ def split_dataset(args, preprocess, target_transform):
 
     return  train_dataset, val_dataset ,test_dataset
 
+# new projection layer
+class proj_layer(nn.Module):
+    def __init__(self, clips_model):
+        super().__init__()
+        self.text_proj = clips_model.text_projection
+        self.image_proj = clips_model.visual.proj
+    def forward(self, image: torch.Tensor, text: torch.Tensor):
+        image_features = image @ self.image_proj
+        text_features = text @ self.text_proj
+        return image_features, text_features
+
+def contrastive_loss(logits_per_image, logits_per_text, margin=1.0):
+    #这个结果很差：猜测可能是前五个的similarities结果并不好并不是positive，similarities are closed 
+    # distance_per_image = margin - logits_per_image
+    # distance_per_text = margin - logits_per_text
+
+    # distance_per_image,_ = torch.sort(distance_per_image, dim=1, descending=False)
+    # distance_per_text,_ = torch.sort(distance_per_text, dim=1, descending=False)
+    
+    # #print(distance_per_image)
+    # # [0.6895, 0.6982, 0.7227,  ..., 0.9053, 0.9058, 0.9399] similarities are closed 
+
+    # # loss of the positive pairs
+    # positive_loss_image = distance_per_image[:, :5].mean()
+    # positive_loss_text = distance_per_text[:, :1].mean()
+
+    # # loss of the negative pairs
+    # negative_loss_image = F.relu(margin - logits_per_image[:, 5:]).mean()
+    # negative_loss_text = F.relu(margin - logits_per_text[:, 1:]).mean()
+
+
+    text_i_matrix = torch.eye(len(logits_per_image)).repeat_interleave(5,dim=0).to(device)
+    image_i_matrix = torch.transpose(text_i_matrix, 0, 1).to(device)
+
+    distance_per_image = margin - logits_per_image
+    distance_per_text = margin - logits_per_text
+
+    positive_loss_image = (distance_per_image * image_i_matrix).mean()
+    positive_loss_text = (distance_per_text * text_i_matrix).mean()
+
+    negative_loss_image = F.relu(distance_per_image * (1-image_i_matrix)).mean()
+    negative_loss_text = F.relu(distance_per_text * (1-text_i_matrix)).mean()
+
+    total_loss= positive_loss_image + negative_loss_image + positive_loss_text + negative_loss_text
+
+    return total_loss/2
+
 #https://github.com/openai/CLIP/issues/57 error using Adam optimizer
 def convert_models_to_fp32(model): 
     for p in model.parameters(): 
         p.data = p.data.float() 
         p.grad.data = p.grad.data.float() 
-
-
-# Added layer
-class ProjectionHead(nn.Module):
-    def __init__(
-        self,
-        embed_dim
-    ):
-        super().__init__()
-        self.linear = nn.Linear(embed_dim, 512)
-        self.gelu = nn.GELU()
-        self.dropout = nn.Dropout(0.1)
-    
-    def forward(self, x):
-        x = self.linear(x)
-        x = self.gelu(x)
-        x = self.dropout(x)
-        return x
-
-class CustomCLIPModel(nn.Module):
-    def __init__(self, clip_model, embed_dim):
-        super(CustomCLIPModel, self).__init__()
-        self.clip_model = clip_model
-        self.additional_layers = ProjectionHead(embed_dim)
-        
-    def forward(self, image, text):
-        # Get features from CLIP
-        image_features = self.clip_model.encode_image(image)
-        text_features = self.clip_model.encode_text(text)
-        # Pass features through additional layers
-        image_features = self.additional_layers(image_features)
-        text_features = self.additional_layers(text_features)
-        return image_features, text_features
 
 
 def main(args):
@@ -77,8 +89,6 @@ def main(args):
     target_transform = lambda texts: clip.tokenize(texts[:5])
 
     if args.evaluate:
-        if args.model_path != "none":
-            model.load_state_dict(torch.load(args.model_path))
         model.eval()
         print("Start evaluating", flush=True)
 
@@ -90,53 +100,18 @@ def main(args):
     train_Loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=False)
     val_loader = DataLoader(dataset=val_dataset, batch_size=args.batch_size, shuffle=False)
 
-    ####################################
-    #change the projection head inside the model
+    for param in model.parameters():
+        param.requires_grad = False
+    
+    model.text_projection.requires_grad = True
+    model.visual.proj.requires_grad = True
 
-    if args.trainable == "new_layer":
-
-        # Create custom model
-        state_dict = model.state_dict()
-        embed_dim = state_dict["text_projection"].shape[1]
-        #print(embed_dim) #512 in b/32 
-        new_model = CustomCLIPModel(model, embed_dim).to(device)
-        clip.model.convert_weights(new_model)
-
-        for param in model.parameters():
-            param.requires_grad = False        
-
-        trainable_params = [p for p in new_model.parameters() if p.requires_grad] 
-
-    if args.trainable == "linear_projection":
-
-        for param in model.parameters():
-            param.requires_grad = False
-            
-        model.text_projection.requires_grad = True
-        model.visual.proj.requires_grad = True
-
-        trainable_params = [p for p in model.parameters() if p.requires_grad]
-
-    if args.trainable == "all":
-
-        trainable_params = [p for p in model.parameters() if p.requires_grad]
-
-    ######################################
-    #change the Optimizer
-         
-    optimizer = optim.AdamW(trainable_params, lr=args.lr, betas=(0.9,0.98), eps=1e-6,weight_decay=1e-3)
-    #optimizer = optim.Adam(trainable_params, lr=args.lr, betas=(0.9,0.98), eps=1e-6,weight_decay=1e-3)
-
-    if args.scheduler:
-        lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.6)  
-    ######################################
-    #Loss function define (change inside the epoch)
+    trainable_params = [p for p in model.parameters() if p.requires_grad]
+    #proj = proj_layer(model)
+    optimizer = optim.Adam(trainable_params, lr=args.lr, betas=(0.9,0.98),eps=1e-6,weight_decay=0.2)
 
     CE_loss = nn.CrossEntropyLoss()
-        
-    def soft_cross_entropy(teacher_logits, student_logits):
-        return -(teacher_logits.softmax(dim=1) * student_logits.log_softmax(dim=1)).sum(dim=1).mean()
-        
+    contrastive_loss = losses.ContrastiveLoss(pos_margin=0.0, neg_margin=1)
 
     #https://github.com/openai/CLIP/issues/57
     def convert_models_to_fp32(model): 
@@ -145,18 +120,11 @@ def main(args):
                 p.data = p.data.float() 
                 p.grad.data = p.grad.data.float() 
 
-    ##################################################
-    #epoch start
-                
     for epoch in range(args.num_epoch):
         total_loss = 0
 
-        if args.trainable == "new_layer":
-            new_model.train()
-        else :
-            model.train()
+        model.train()
         print("start to train")
-
         for images, texts in tqdm(train_Loader):
             optimizer.zero_grad()
 
@@ -168,71 +136,58 @@ def main(args):
             texts = texts.to(device)
 
             #encoding & cosine similarity as logits       
-            logits_per_image, logits_per_text = model(images, texts)
+            #logits_per_image, logits_per_text = model(images, texts)
+            
+            #encoding & cosine similarity as logits       
+            image_encodings = model.encode_image(images)
+            text_encodings = model.encode_text(texts)
+            
+            # Normalise 
+            image_encodings = image_encodings / image_encodings.norm(dim=-1, keepdim=True)
+            text_encodings = text_encodings / text_encodings.norm(dim=-1, keepdim=True)
 
-            #encoding & cosine similarity as logits 
-
-            # if args.trainable == "new_layer":
-            #     image_encodings, text_encodings = new_model(images, texts)
-            # else :                 
-            #     image_encodings = model.encode_image(images)
-            #     text_encodings = model.encode_text(texts)
-
-            # temperature = 0.07
-            # logits_per_image = (image_encodings @ text_encodings.t()) / temperature
-            # logits_per_text = logits_per_image.T
-                
-            ##############################################
-            #Change the loss function
-            if args.loss == "soft_cross_entropy":
-                teacher_logits_image = logits_per_image @ logits_per_image.T
-                teacher_logits_text = logits_per_text @ logits_per_text.T
-
-                image_loss = soft_cross_entropy(teacher_logits_image, logits_per_image)
-                text_loss = soft_cross_entropy(teacher_logits_text, logits_per_text)
-                loss = (image_loss + text_loss) / 2
-
+            temperature = 0.07
+            logits_per_image = (image_encodings @ text_encodings.T)/ temperature
+            logits_per_text = logits_per_image.T
+            
             if args.loss == "cross_entropy" :
                 targets = torch.arange(len(images),dtype=torch.long, device=device)
 
                 image_loss = CE_loss(logits_per_image, targets)
                 text_loss  = CE_loss(logits_per_text, targets)
                 loss = (image_loss + text_loss)/2
+
+            if args.loss == "contrastive" :
+                
+                #使用pytorch_metric_learning库结果和我自己写的第一个一样很差，猜测可能是相似值过于靠近
+                targets = torch.arange(len(images))
+
+
+                image_loss = contrastive_loss(logits_per_image , targets)
+                text_loss = contrastive_loss(logits_per_text , targets)
+                loss = (image_loss + text_loss)/2
+            
+                # loss = contrastive_loss(logits_per_image, logits_per_text)
+
             
             loss.backward()
 
-            total_loss += loss.item()
-
-            optimizer.step()
+            total_loss += loss
 
             if device == "cpu":
                 optimizer.step()
-                
             else : 
-                if args.trainable == "new_layer":
-                    convert_models_to_fp32(new_model)
-                else :
-                    convert_models_to_fp32(model)
+                convert_models_to_fp32(model)
                 optimizer.step()
                 clip.model.convert_weights(model)
-                if args.trainable == "new_layer":
-                    clip.model.convert_weights(new_model)
 
+        avg_loss = total_loss / len(train_Loader)
+        print(f"Epoch {epoch+1}/{args.num_epoch} has done, Average Loss: {avg_loss}")
 
-        if args.trainable == "new_layer":
-            new_model.eval()
-            print("start to evaluate")
-            Evaluation.metrics_at_k(new_model, val_loader, k_vals= k_vals, batch_size=16)
-        else:
-            model.eval()
-            print("start to evaluate")
-            Evaluation.metrics_at_k(model, val_loader, k_vals= k_vals, batch_size=16)
-        
-        if args.scheduler:
-            lr_scheduler.step()
+        model.eval()
+        print("start to evaluate")
+        Evaluation.metrics_at_k(model, val_loader, k_vals= k_vals, batch_size=16)
 
-        avg_train_loss = total_loss / len(train_Loader)
-        print(f"Training Loss: {avg_train_loss:.4f}")
 
 if __name__ == '__main__':
     args = parser.parse_arguments() #read the parameters from parser

cb6f34f7161c563e2c302f64c6840af5bc4287ce
Huan Xiao <389031426@qq.com>
2024-01-03 11:41:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from tqdm import tqdm
 import torch
 import torch.nn as nn
@@ -31,83 +30,18 @@ def split_dataset(args, preprocess, target_transform):
 
     return  train_dataset, val_dataset ,test_dataset
 
-#https://github.com/openai/CLIP/issues/57 error using Adam optimizer
-def convert_models_to_fp32(model): 
-    for p in model.parameters(): 
-        p.data = p.data.float() 
-        p.grad.data = p.grad.data.float() 
-
-
-# Added layer
-class ProjectionHead(nn.Module):
-    def __init__(
-        self,
-        embed_dim
-    ):
-        super().__init__()
-        self.linear = nn.Linear(embed_dim, 512)
-        self.gelu = nn.GELU()
-        self.dropout = nn.Dropout(0.1)
-    
-    def forward(self, x):
-        x = self.linear(x)
-        x = self.gelu(x)
-        x = self.dropout(x)
-        return x
-
-class CustomCLIPModel(nn.Module):
-    def __init__(self, clip_model, embed_dim):
-        super(CustomCLIPModel, self).__init__()
-        self.clip_model = clip_model
-        self.additional_layers = ProjectionHead(embed_dim)
-        
-    def forward(self, image, text):
-        # Get features from CLIP
-        image_features = self.clip_model.encode_image(image)
-        text_features = self.clip_model.encode_text(text)
-        # Pass features through additional layers
-        image_features = self.additional_layers(image_features)
-        text_features = self.additional_layers(text_features)
-        return image_features, text_features
-
-
 def main(args):
     
     k_vals = [1,5,10]
     model, preprocess = clip.load(args.model, device=device)
     target_transform = lambda texts: clip.tokenize(texts[:5])
 
-    if args.evaluate:
-        if args.model_path != "none":
-            model.load_state_dict(torch.load(args.model_path))
-        model.eval()
-        print("Start evaluating", flush=True)
-
-        _,_,eva_dataset = split_dataset(args,preprocess,target_transform)
-        eva_Loader = DataLoader(dataset=eva_dataset, batch_size=args.batch_size, shuffle=False)
-        Evaluation.metrics_at_k(model, eva_Loader, k_vals= k_vals, batch_size=16)
-
-    train_dataset, val_dataset, test_dataset = split_dataset(args,preprocess,target_transform)
+    train_dataset, val_dataset,_ = split_dataset(args,preprocess,target_transform)
     train_Loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=False)
     val_loader = DataLoader(dataset=val_dataset, batch_size=args.batch_size, shuffle=False)
-    test_loader = DataLoader(dataset=test_dataset, batch_size=args.batch_size, shuffle=False)
 
     ####################################
     #change the projection head inside the model
-
-    if args.trainable == "new_layer":
-
-        # Create custom model
-        state_dict = model.state_dict()
-        embed_dim = state_dict["text_projection"].shape[1]
-        #print(embed_dim) #512 in b/32 
-        new_model = CustomCLIPModel(model, embed_dim).to(device)
-        clip.model.convert_weights(new_model)
-
-        for param in model.parameters():
-            param.requires_grad = False        
-
-        trainable_params = [p for p in new_model.parameters() if p.requires_grad] 
 
     if args.trainable == "linear_projection":
 
@@ -118,39 +52,23 @@ def main(args):
         model.visual.proj.requires_grad = True
 
         trainable_params = [p for p in model.parameters() if p.requires_grad]
-
-    if args.trainable == "all":
-
-        trainable_params = [p for p in model.parameters() if p.requires_grad]
-
-    ######################################
+  ######################################
     #change the Optimizer
          
     optimizer = optim.AdamW(trainable_params, lr=args.lr, betas=(0.9,0.98), eps=1e-6,weight_decay=1e-3)
-    #optimizer = optim.Adam(trainable_params, lr=args.lr, betas=(0.9,0.98), eps=1e-6,weight_decay=1e-3)
+   ######################################
+    #Loss function define (change inside the epoch)
 
-    if args.scheduler:
-        lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.6)  
+    CE_loss = nn.CrossEntropyLoss()
         
-    #https://github.com/openai/CLIP/issues/57
-    def convert_models_to_fp32(model): 
-        for p in model.parameters(): 
-            if p.requires_grad:
-                p.data = p.data.float() 
-                p.grad.data = p.grad.data.float() 
-
     ##################################################
     #epoch start
-    #best_val_loss = float('inf')            
+                
     for epoch in range(args.num_epoch):
-
         total_loss = 0
 
-        if args.trainable == "new_layer":
-            new_model.train()
-        else :
-            model.train()
-        print(f"start the {epoch+1}/{args.num_epoch} epoch training")
+        model.train()
+        print("start to train")
 
         for images, texts in tqdm(train_Loader):
             optimizer.zero_grad()
@@ -165,23 +83,12 @@ def main(args):
             #encoding & cosine similarity as logits       
             logits_per_image, logits_per_text = model(images, texts)
 
-            #encoding & cosine similarity as logits 
+            if args.loss == "cross_entropy" :
+                targets = torch.arange(len(images),dtype=torch.long, device=device)
 
-            # if args.trainable == "new_layer":
-            #     image_encodings, text_encodings = new_model(images, texts)
-            # else :                 
-            #     image_encodings = model.encode_image(images)
-            #     text_encodings = model.encode_text(texts)
-
-            # temperature = 0.07
-            # logits_per_image = (image_encodings @ text_encodings.t()) / temperature
-            # logits_per_text = logits_per_image.T
-
-            targets = torch.arange(len(images),dtype=torch.long, device=device)
-            CE_loss = nn.CrossEntropyLoss()
-            image_loss = CE_loss(logits_per_image, targets)
-            text_loss  = CE_loss(logits_per_text, targets)
-            loss = (image_loss + text_loss)/2
+                image_loss = CE_loss(logits_per_image, targets)
+                text_loss  = CE_loss(logits_per_text, targets)
+                loss = (image_loss + text_loss)/2
             
             loss.backward()
 
@@ -189,83 +96,12 @@ def main(args):
 
             optimizer.step()
 
-            # if device == "cpu":
-            #     optimizer.step()
-                
-            # else : 
-            #     if args.trainable == "new_layer":
-            #         convert_models_to_fp32(new_model)
-            #     else :
-            #         convert_models_to_fp32(model)
-                
-            #     optimizer.step()
-            #     clip.model.convert_weights(model)
-            #     if args.trainable == "new_layer":
-            #         clip.model.convert_weights(new_model)
+        model.eval()
+        print("start to evaluate")
+        Evaluation.metrics_at_k(model, val_loader, k_vals= k_vals, batch_size=16)
 
         avg_train_loss = total_loss / len(train_Loader)
         print(f"Training Loss: {avg_train_loss:.4f}")
-
-        model.eval()
-        # total_val_loss = 0
-        # print(f"Epoch {epoch+1}/{args.num_epoch} - Validation")
-
-        # for images, texts in tqdm(val_loader):
-        #     #texts: batch size x 77
-        #     random_indices = torch.randint(0, 5, (len(images),))
-        #     texts = torch.stack([texts[i, idx] for i, idx in enumerate(random_indices)])
-
-        #     images = images.to(device)
-        #     texts = texts.to(device)
-        #     #encoding & cosine similarity as logits       
-        #     logits_per_image, logits_per_text = model(images, texts)
-
-        #     #encoding & cosine similarity as logits 
-        #     # if args.trainable == "new_layer":
-        #     #     image_encodings, text_encodings = new_model(images, texts)
-        #     # else :                 
-        #     #     image_encodings = model.encode_image(images)
-        #     #     text_encodings = model.encode_text(texts)
-
-        #     # temperature = 0.07
-        #     # logits_per_image = (image_encodings @ text_encodings.t()) / temperature
-        #     # logits_per_text = logits_per_image.T
-
-        #     targets = torch.arange(len(images),dtype=torch.long, device=device)
-        #     CE_loss = nn.CrossEntropyLoss()
-        #     image_loss = CE_loss(logits_per_image, targets)
-        #     ext_loss  = CE_loss(logits_per_text, targets)
-        #     loss = (image_loss + text_loss)/2
-
-        #     total_val_loss += val_loss.item()
-            
-        # ######
-        # avg_val_loss = total_val_loss / len(val_loader)
-        # print(f"Val Loss: {avg_val_loss:.4f}")
-
-        # if avg_val_loss < best_val_loss:
-        #     best_val_loss = avg_val_loss
-        #     best_model = model.state_dict()
-
-        print("start to test for this epoch")
-        if args.trainable == "new_layer":
-            Evaluation.metrics_at_k(new_model, val_loader, k_vals= k_vals, batch_size=16)
-        else:
-            Evaluation.metrics_at_k(model, val_loader, k_vals= k_vals, batch_size=16)
-                
-        if args.scheduler:
-            lr_scheduler.step()
-
-    
-    torch.save(model.state_dict(), '/kaggle/working/best_model.pth')
-    print("save the best model")
-
-    model, _ = clip.load(args.model, device=device)
-    model.load_state_dict(torch.load('/kaggle/working/best_model.pth'))
-    print("start to test:")
-    Evaluation.metrics_at_k(model, test_loader, k_vals= k_vals, batch_size=16)
-
-
 
 if __name__ == '__main__':
     args = parser.parse_arguments() #read the parameters from parser

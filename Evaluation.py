f2ba030af374450809a9bc55d6d627e16155ef04
Huan Xiao <389031426@qq.com>
2024-01-12 11:59:27 +0100

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -48,13 +48,13 @@ def encode_dataset(clip, eva_Loader, batch_size = 16):
             # 16 x image(3*224*224)???
             text = torch.flatten(text, start_dim=0, end_dim=1)
 
-            if args.trainable == "new_layer":
-                image_features, text_features = clip(images, text)
-                image_encodings.append(image_features)
-                text_encodings.append(text_features)
-            else:
-                image_encodings.append(clip.encode_image(images))
-                text_encodings.append(clip.encode_text(text))
+            # if args.trainable == "adaptor":
+            #     image_features, text_features = clip(images, text)
+            #     image_encodings.append(image_features)
+            #     text_encodings.append(text_features)
+            # else:
+            image_encodings.append(clip.encode_image(images))
+            text_encodings.append(clip.encode_text(text))
 
         image_encodings = torch.cat(image_encodings)
         text_encodings = torch.cat(text_encodings)
